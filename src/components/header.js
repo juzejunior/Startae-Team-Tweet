1bbb1a16343ab4f7ee285e152c0de6c081fb6bfc
Jr <jr>
2020-01-10 03:27:47 -0300

    wip: mobile state trasnformed into state

--- src/components/header.js
+++ src/components/header.js
@@ -1,56 +1,48 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useEffect, useState } from "react"
 import StartaeLogoSymbol from "../images/logo-symbol.svg"
 import StartaeLogoType from "../images/logo-type.svg"
-import {
-  BrowserView,
-  MobileView,
-  isBrowser,
-  isMobile,
-} from "react-device-detect"
+import { isMobile } from "react-device-detect"
 
-const Header = () => (
-  <Link to={"/"}>
-    {/*isMobile && (
+function Header() {
+  const [mobileState, setMobileState] = useState(false)
+
+  useEffect(() => {
+    setMobileState(isMobile)
+  }, [])
+
+  return (
+    <Link to={"/"}>
       <div
+        key={mobileState}
         style={{
           display: "flex",
-          justifyContent: "center",
-          height: 70,
+          marginBottom: isMobile ? "auto" : 64,
+          marginLeft: isMobile ? "auto" : 16,
+          marginRight: isMobile ? "auto" : 16,
+          background: isMobile ? "#550aaa" : "#5D1EB2",
+          position: isMobile ? "fixed" : "static",
           width: "100%",
-          position: "fixed",
-          background: "#550aaa",
-          alignItems: "center",
+          justifyContent: isMobile ? "center" : "flex-start",
+          alignItems: isMobile ? "center" : "start",
         }}
       >
-        <img src={StartaeLogoSymbol} style={{ position: "relative" }} />
+        <img
+          src={StartaeLogoSymbol}
+          style={{ marginRight: 8, marginTop: 24 }}
+        />
+        <img
+          src={StartaeLogoType}
+          style={{
+            marginRight: 8,
+            marginTop: 24,
+            display: isMobile ? "none" : "inline",
+          }}
+        />
       </div>
-      )*/}
-    <div
-      style={{
-        display: "flex",
-        marginBottom: isMobile ? "auto" : 64,
-        marginLeft: isMobile ? "auto" : 16,
-        marginRight: isMobile ? "auto" : 16,
-        background: isMobile ? "#550aaa" : "#5D1EB2",
-        position: isMobile ? "fixed" : "static",
-        width: "100%",
-        justifyContent: isMobile ? "center" : "flex-start",
-        alignItems: isMobile ? "center" : "start",
-      }}
-    >
-      <img src={StartaeLogoSymbol} style={{ marginRight: 8, marginTop: 24 }} />
-      <img
-        src={StartaeLogoType}
-        style={{
-          marginRight: 8,
-          marginTop: 24,
-          display: isMobile ? "none" : "inline",
-        }}
-      />
-    </div>
-  </Link>
-)
+    </Link>
+  )
+}
 
 export default Header
